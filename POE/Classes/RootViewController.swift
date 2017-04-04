7826c05b6b441ca38d1895d423c31e509d6945f3
Benjamin Erhart <be@benjaminerhart.com>
2017-04-04 14:55:37 +0200

    Improved UIPageController stuff to start looking more like the desired intro.

--- POE/Classes/RootViewController.swift
+++ POE/Classes/RootViewController.swift
@@ -12,12 +12,13 @@ public class RootViewController: XibViewController, UIPageViewControllerDelegate
 
     var pageViewController: UIPageViewController?
 
+    @IBOutlet weak var pageControl: UIPageControl!
 
     override public func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Configure the page view controller and add it as a child view controller.
-        self.pageViewController = UIPageViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal, options: nil)
+        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         self.pageViewController!.delegate = self
 
         let startingViewController: DataViewController = self.modelController.viewControllerAtIndex(0)!
@@ -30,21 +31,13 @@ public class RootViewController: XibViewController, UIPageViewControllerDelegate
         self.view.addSubview(self.pageViewController!.view)
 
         // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
-        var pageViewRect = self.view.bounds
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            pageViewRect = pageViewRect.insetBy(dx: 40.0, dy: 40.0)
-        }
-        self.pageViewController!.view.frame = pageViewRect
+        self.pageViewController!.view.frame = self.view.bounds
+            .insetBy(dx: 0, dy: 25).offsetBy(dx: 0, dy: -25)
 
         self.pageViewController!.didMove(toParentViewController: self)
 
         // Replace standard font with our corporate design font: Roboto
         robotoIt()
-    }
-
-    override public func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     var modelController: ModelController {
@@ -58,36 +51,46 @@ public class RootViewController: XibViewController, UIPageViewControllerDelegate
 
     var _modelController: ModelController? = nil
 
+    @IBAction func nextPage() {
+        var nextIndex = 0
+
+        if let dataViewController = pageViewController?.viewControllers?[0] {
+            let index = modelController.indexOfViewController(dataViewController as! DataViewController)
+
+            if index == NSNotFound || index >= modelController.pageData.count - 1 {
+                nextIndex = 0
+            }
+            else {
+                nextIndex = index + 1
+            }
+        }
+
+        let pendingViewControllers: [UIViewController] = [modelController.viewControllerAtIndex(nextIndex)!]
+
+        pageViewController(pageViewController!, willTransitionTo: pendingViewControllers)
+
+        pageViewController?.setViewControllers(pendingViewControllers, direction: .forward,
+                                               animated: true, completion: nil)
+    }
+    
     // MARK: - UIPageViewController delegate methods
 
-    public func pageViewController(_ pageViewController: UIPageViewController, spineLocationFor orientation: UIInterfaceOrientation) -> UIPageViewControllerSpineLocation {
-        if (orientation == .portrait) || (orientation == .portraitUpsideDown) || (UIDevice.current.userInterfaceIdiom == .phone) {
-            // In portrait orientation or on iPhone: Set the spine position to "min" and the page view controller's view controllers array to contain just one view controller. Setting the spine position to 'UIPageViewControllerSpineLocationMid' in landscape orientation sets the doubleSided property to true, so set it to false here.
-            let currentViewController = self.pageViewController!.viewControllers![0]
-            let viewControllers = [currentViewController]
-            self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: true, completion: {done in })
+    public func pageViewController(_ pageViewController: UIPageViewController,
+                                   willTransitionTo pendingViewControllers: [UIViewController]) {
 
-            self.pageViewController!.isDoubleSided = false
-            return .min
-        }
-
-        // In landscape orientation: Set set the spine location to "mid" and the page view controller's view controllers array to contain two view controllers. If the current page is even, set it to contain the current and next view controllers; if it is odd, set the array to contain the previous and current view controllers.
-        let currentViewController = self.pageViewController!.viewControllers![0] as! DataViewController
-        var viewControllers: [UIViewController]
-
-        let indexOfCurrentViewController = self.modelController.indexOfViewController(currentViewController)
-        if (indexOfCurrentViewController == 0) || (indexOfCurrentViewController % 2 == 0) {
-            let nextViewController = self.modelController.pageViewController(self.pageViewController!, viewControllerAfter: currentViewController)
-            viewControllers = [currentViewController, nextViewController!]
-        } else {
-            let previousViewController = self.modelController.pageViewController(self.pageViewController!, viewControllerBefore: currentViewController)
-            viewControllers = [previousViewController!, currentViewController]
-        }
-        self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: true, completion: {done in })
-
-        return .mid
+        pageControl.currentPage = modelController.indexOfViewController(pendingViewControllers[0] as! DataViewController)
     }
 
 
-}
+    public func pageViewController(_ pageViewController: UIPageViewController,
+                                   spineLocationFor orientation: UIInterfaceOrientation)
+        -> UIPageViewControllerSpineLocation {
 
+        let currentViewController = self.pageViewController!.viewControllers![0]
+        let viewControllers = [currentViewController]
+        self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: true, completion: {done in })
+
+        self.pageViewController!.isDoubleSided = false
+        return .min
+    }
+}
