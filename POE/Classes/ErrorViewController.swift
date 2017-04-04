7c2b5109a6149f3608fc80a2d6635dcedc213b10
Benjamin Erhart <be@benjaminerhart.com>
2017-04-04 15:41:52 +0200

    Added an ErrorViewController.

---
+++ POE/Classes/ErrorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorViewController.swift
+//  Pods
+//
+//  Created by Benjamin Erhart on 04.04.17.
+//
+//
+
+import UIKit
+
+public class ErrorViewController: XibViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Replace standard font with our corporate design font: Roboto
+        robotoIt()
+    }
+}
