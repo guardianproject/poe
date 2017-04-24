538f8358d42c84f8536d9ad0d0e24cdad24e198e
Benjamin Erhart <be@benjaminerhart.com>
2017-04-24 15:48:33 +0200

    Replaced UIActivityIndicator with UIProgressView in ErrorViewController, analogue to ConnectingViewController.

--- POE/Classes/ErrorViewController.swift
+++ POE/Classes/ErrorViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 public class ErrorViewController: XibViewController, POEDelegate {
 
+    @IBOutlet weak var progressView: UIProgressView!
+
+    /**
+        Update the progress view with the given progress.
+
+        Default is 50%.
+
+        - parameter progress: The new progress value. See [UIProgressView#setProgress(_:animated:)](https://developer.apple.com/reference/uikit/uiprogressview/1619846-setprogress)
+     */
+    public func updateProgress(_ progress: Float) {
+        progressView.setProgress(progress, animated: false)
+    }
+
     // MARK: - POEDelegate pass-through
 
     public func introFinished(_ useBridge: Bool) {
